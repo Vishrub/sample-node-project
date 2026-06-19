bb71ff17da85e4410772665453be08d0ef0ac0ca
Vishrub <102487121+Vishrub@users.noreply.github.com>
2026-06-19 17:10:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-// Importing required packages
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000); // Application port is set
-app.set('views', __dirname + '/app/server/views'); // Views folder is set
-app.set('view engine', 'ejs'); // View engine is set
-app.use(express.static(__dirname + '/app/public')); // Public folder containing static files is set
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/app/public'));
 
-require('./app/routes')(app); // Routes are imported
+require('./app/routes')(app);
 
-const server = http.createServer(app).listen(app.get('port'), function(){
-	console.log('The application is running on port ' + app.get('port'));
-}); // Http server is created
-
-module.exports = server;
+module.exports = app;
